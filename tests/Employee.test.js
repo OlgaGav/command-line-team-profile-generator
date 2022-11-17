9b8c5a9b8b90a0cff7d13319096d9caeda664cdb
Olga Gavrushenko <ogavby@gmail.com>
2022-11-17 08:42:27 -0800

    added unit tests for Employee object with implementation

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -17,73 +17,162 @@ describe("Employee", () => {
       expect(employee.email).toBe(email);
     });
 
-    test("[Negative test] employee object can't be created with undefined name", async () => {
+    test("Employee object can't be created with undefined name", async () => {
       // test setup
-      const id = "123";
-      const name = 'undefined';
-      const email = "test@test.com";
+      const name = "undefined";
 
       // running the code
       function testThrowingError() {
         try {
-          const employee = new Employee(id, name, email);
+          const employee = new Employee("123", name, "test@test.com");
         } catch (error) {
           return error;
         }
       }
 
       // verify the result
-      expect(testThrowingError()).toBe("Verify name value. It can not be null or empty.");
+      expect(testThrowingError()).toBe(
+        "Invalid name value provided. It can not be null or empty."
+      );
     });
 
-    test("[Negative test] employee object can't be created with empty name", async () => {
+    test("Employee object can't be created with empty name", async () => {
       // test setup
-      const id = "123";
       const name = "";
-      const email = "test@test.com";
 
       // running the code
       function testThrowingError() {
         try {
-          const employee = new Employee(id, name, email);
+          const employee = new Employee("123", name, "test@test.com");
         } catch (error) {
           return error;
         }
       }
 
       // verify the result
-      expect(testThrowingError()).toBe("Verify name value. It can not be null or empty.");
+      expect(testThrowingError()).toBe(
+        "Invalid name value provided. It can not be null or empty."
+      );
     });
 
-    test("[Negative test] employee can't be created with null name", async () => {
+    test("Employee can't be created with null name", async () => {
       // test setup
-      const id = "123";
       const name = null;
-      const email = "test@test.com";
 
       // running the code
       function testThrowingError() {
         try {
-          const employee = new Employee(id, name, email);
+          const employee = new Employee("123", name, "test@test.com");
         } catch (error) {
           return error;
         }
       }
 
       // verify the result
-      expect(testThrowingError()).toBe("Verify name value. It can not be null or empty.");
+      expect(testThrowingError()).toBe(
+        "Invalid name value provided. It can not be null or empty."
+      );
     });
 
-    // test("[Negative test] employee can't be created without id", async () => {});
-    // test("[Negative test] employee can't be created with empty id", async () => {});
-    // test("[Negative test] employee can't be created with null id", async () => {});
-    // test("[Negative test] employee can't be created without email", async () => {});
-    // test("[Negative test] employee can't be created with empty email", async () => {});
-    // test("[Negative test] employee can't be created with null email", async () => {});
+    test("Employee can't be created with undefined id", async () => {
+      // test setup
+      const id = "undefined";
+      // running the code
+      function testThrowingError() {
+        try {
+          const employee = new Employee(id, "John Smith", "test@test.com");
+        } catch (error) {
+          return error;
+        }
+      }
+      // verify the result
+      expect(testThrowingError()).toBe(
+        "Invalid ID value provided. It can not be null or empty."
+      );
+    });
+    test("Employee can't be created with empty id", async () => {
+      // test setup
+      const id = "";
+      // running the code
+      function testThrowingError() {
+        try {
+          const employee = new Employee(id, "John Smith", "test@test.com");
+        } catch (error) {
+          return error;
+        }
+      }
+      // verify the result
+      expect(testThrowingError()).toBe(
+        "Invalid ID value provided. It can not be null or empty."
+      );
+    });
+    test("Employee can't be created with null id", async () => {
+      // test setup
+      const id = null;
+      // running the code
+      function testThrowingError() {
+        try {
+          const employee = new Employee(id, "John Smith", "test@test.com");
+        } catch (error) {
+          return error;
+        }
+      }
+      // verify the result
+      expect(testThrowingError()).toBe(
+        "Invalid ID value provided. It can not be null or empty."
+      );
+    });
+    test("Employee can't be created with undefined email", async () => {
+      // test setup
+      const email = "undefined";
+      // running the code
+      function testThrowingError() {
+        try {
+          const employee = new Employee("123", "John Smith", email);
+        } catch (error) {
+          return error;
+        }
+      }
+      // verify the result
+      expect(testThrowingError()).toBe(
+        "Invalid email value provided. It can not be null or empty."
+      );
+    });
+    test("Employee can't be created with empty email", async () => {
+      // test setup
+      const email = "";
+      // running the code
+      function testThrowingError() {
+        try {
+          const employee = new Employee("123", "John Smith", email);
+        } catch (error) {
+          return error;
+        }
+      }
+      // verify the result
+      expect(testThrowingError()).toBe(
+        "Invalid email value provided. It can not be null or empty."
+      );
+    });
+    test("Employee can't be created with null email", async () => {
+      // test setup
+      const email = null;
+      // running the code
+      function testThrowingError() {
+        try {
+          const employee = new Employee("123", "John Smith", email);
+        } catch (error) {
+          return error;
+        }
+      }
+      // verify the result
+      expect(testThrowingError()).toBe(
+        "Invalid email value provided. It can not be null or empty."
+      );
+    });
   });
 
   describe("Validate methods of Employee object", () => {
-
     test("Method getName() return the valid name of the Employee", async () => {
       // setup
       const name = "John Smith";
@@ -130,6 +219,5 @@ describe("Employee", () => {
       // verify the result
       expect(role).toBe("Employee");
     });
-
   });
 });
