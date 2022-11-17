a7dc29d63fb6dab9a25773518f770807772a00af
Olga Gavrushenko <ogavby@gmail.com>
2022-11-17 09:43:53 -0800

    implemented subclasses with unit tests

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -1,10 +1,55 @@
 const Engineer = require("../lib/Engineer");
 
-describe('Engineer', () => {
-    describe('Initialize Engineer object with parameters: name, id, email', () => {
-        // setup
-        let githubUsername = 'testuser';
-        // run
-        // validation
-    })
-})
+describe("Engineer", () => {
+  describe("Initialize Engineer object", () => {
+    test("Create Engineer object with parameters: id, name, email, github", async () => {
+      // setup
+      const id = "123";
+      const name = "John Smith";
+      const email = "test@test.com";
+      const github = "testuser";
+      // running the code
+      const engineer = new Engineer(id, name, email, github);
+      // validation
+      expect(engineer.id).toBe(id);
+      expect(engineer.name).toBe(name);
+      expect(engineer.email).toBe(email);
+      expect(engineer.github).toBe(github);
+    });
+  });
+
+  describe("Validate methods of Engineer object", () => {
+    test("Method getGithub() return the valid github value of the Engineer", async () => {
+      // setup
+      const github = "test@test.com";
+      const engineer = new Engineer(
+        "123",
+        "John Smith",
+        "test@test.com",
+        github
+      );
+
+      // running the code
+      const actual_github = engineer.getGithub();
+
+      // verify the result
+      expect(actual_github).toBe(github);
+    });
+
+    test("Method getRole() return the valid role of the Engineer", async () => {
+      // setup
+      const engineer = new Engineer(
+        "123",
+        "John Smith",
+        "test@test.com",
+        "smith-github"
+      );
+
+      // running the code
+      const role = engineer.getRole();
+
+      // verify the result
+      expect(role).toBe("Engineer");
+    });
+  });
+});
